f902822890e341790802d7f32f57bf19939c946c
Davide Pulci <davide.pulci@gmail.com>
2022-04-09 16:56:01 +0200

    inizio, creati componenti base

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    MainComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/style/general";
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Montserrat", sans-serif;
+}
+
+body {
+  background: #1e2d3b;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+};
+</script>
+
+<style>
+</style>
