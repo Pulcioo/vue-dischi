549a9440dacf7854bc380d2584e49989c29c8281
Davide Pulci <davide.pulci@gmail.com>
2022-04-11 21:40:50 +0200

    aggiunta select per filtrare genere

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container py-5">
     <div class="row">
+      <div class="col-12">
+        <SelectedGenre @searchByGenre="filterByGenre" />
+      </div>
       <div
         v-if="albums.length > 0"
-        class="col-12 d-flex flex-wrap justify-content-center m"
+        class="col-12 d-flex flex-wrap justify-content-center"
       >
-        <CardAlbum v-for="(item, index) in albums" :key="index" :album="item" />
+        <CardAlbum
+          v-for="(item, index) in filteredList"
+          :key="index"
+          :album="item"
+        />
       </div>
     </div>
   </div>
@@ -14,11 +21,13 @@
 <script>
 import axios from "axios";
 import CardAlbum from "@/components/CardAlbum.vue";
+import SelectedGenre from "@/components/SelectedGenre.vue";
 
 export default {
   name: "MainComponent",
   components: {
     CardAlbum,
+    SelectedGenre,
   },
   props: {
     url: String,
@@ -26,6 +35,7 @@ export default {
   data() {
     return {
       albums: [],
+      genre: "",
     };
   },
   mounted() {
@@ -33,6 +43,19 @@ export default {
       this.albums = response.data.response;
       console.log(this.albums[0]);
     });
+  },
+  computed: {
+    filteredList() {
+      const filter = this.albums.filter((item) => {
+        return item.genre.includes(this.genre);
+      });
+      return filter;
+    },
+  },
+  methods: {
+    filterByGenre(genre) {
+      this.genre = genre;
+    },
   },
 };
 </script>
---
+++ src/components/SelectedGenre.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <select @change="showGenre($event)">
+      <option value="">All</option>
+      <option value="Rock">Rock</option>
+      <option value="Pop">Pop</option>
+      <option value="Metal">Metal</option>
+      <option value="Jazz">Jazz</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectGenre",
+  data() {
+    return {
+      selectGenre: "",
+    };
+  },
+  methods: {
+    showGenre(event) {
+      console.log(event.target.value);
+      this.selectGenre = event.target.value;
+      this.$emit("searchByGenre", this.selectGenre);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
